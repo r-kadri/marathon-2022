0553c786f091c86d03fcc6b39bbf1da8cc5fac93
Sebastien Tabary <sebastien.tabary@univ-artois.fr>
2022-12-02 15:54:59 +0100

    update deploiement scripts

--- resources/binaries/install.sh
+++ resources/binaries/install.sh
@@ -2,10 +2,10 @@
 
 echo "#begin install"
 cd ~/www
-if [ ! -d ./vendor ]
+if [ ! -d ./vendor ] ; then
   composer install
 fi
-if [ ! -d ./node_modules ]
+if [ ! -d ./node_modules ] ; then
   npm install && npm run build
 fi
 fixRights
