050dd50adee4f4e13356c101175eda4030ba489e
Sebastien Tabary <sebastien.tabary@univ-artois.fr>
2022-12-02 15:20:06 +0100

    ajout ci/cd - update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 #eval `ssh-agent -s`
 #echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add -
 
-echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www/vendor"
+echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www/vendor ;  echo $?"
 echo $?
 
-echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www"
+echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www ; echo $?"
 echo $?
 
 #composer install
