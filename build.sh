4b541b05d9c6b1a9fab5281b62f5ac395ff88677
Sebastien Tabary <sebastien.tabary@univ-artois.fr>
2022-12-02 15:15:31 +0100

    ajout ci/cd

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+#eval `ssh-agent -s`
+#echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add -
+
+echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www/vendor"
+echo $?
+
+echo "ssh $NAME@172.31.146.106 test -d  /srv/comptes/marathon22/$NAME/www"
+echo $?
+
+#composer install
+#npm install && npm run build
+#rsync -rtv . $NOM@hemery-virtual-machine:/var/www/html/robert
