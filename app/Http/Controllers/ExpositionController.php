bc9bcb6949dab335048d25720cfb3a9222088896
Clement.mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2022-12-14 10:21:53 +0100

    base du projet (controller fait + index.blade)

---
+++ app/Http/Controllers/ExpositionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Env\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class ExpositionController extends Controller
+{
+    public function index(Request $request){
+
+    }
+}
