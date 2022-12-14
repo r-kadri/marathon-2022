16d821800140c9c371b3da6c5dc064f900be3e14
lucas.broda <lucas_broda@ens.univ-artois.fr>
2022-12-14 15:27:04 +0100

    Ajout de contenu dans la vue create.blade

--- resources/views/exposition/create.blade.php
+++ resources/views/exposition/create.blade.php
@@ -1,1 +1,52 @@
+<form action="{{route('exposition.store')}}" method="POST" enctype="multipart/form-data">
+    {!! csrf_field() !!}
+    <div class="text-center" style="margin-top: 2rem">
+        <h3>Création d'une oeuvre</h3>
+        <hr class="mt-2 mb-2">
+    </div>
+    <div>
+        {{-- Nom  --}}
+        <label for="nom"><strong>Nom : </strong></label>
+        <input type="text" class="form-control" name="nom" id="nom">
 
+    </div>
+    <div>
+        {{-- Media_url --}}
+        <label for="media_url"><strong>Media_url : </strong></label>
+        <input type="url" class="form-control" id="media_url" name="media_url">
+
+    </div>
+    <div>
+        {{-- Thumbnail_url --}}
+        <label for="thumbnail_url"><strong>Thumbnail_url : </strong></label>
+        <input type="url" class="form-control" id="thumbnail_url" name="thumbnail_url">
+
+    </div>
+
+    <div>
+        {{-- Auteur --}}
+        <label for="author"><strong>Auteur : </strong></label>
+        <input type="text" class="form-control" id="author" name="author">
+
+    </div>
+
+    <div>
+        {{-- Date de création --}}
+        <label for="date_de_creation"><strong>Date De Création : </strong></label>
+        <input type="date" class="form-control" id="date_de_creation" name="date_de_creation">
+
+    </div>
+
+    <div>
+        {{-- Description --}}
+        <label for="description"><strong>Description : </strong></label>
+        <input type="text" class="form-control" id="description" name="description">
+
+    </div>
+
+
+    <div>
+        <button class="btn btn-success" type="submit">Valider</button>
+    </div>
+</form>
+<h2><a href="{{ route('exposition.index') }}">retour</a></h2>
